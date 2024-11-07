bb1d84a6cd3fed55e66ccc805c784ee7c662e287
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2024-11-07 15:36:26 +0100

    GameLab project initialization and add Biome

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return <div>Coucou</div>;
+}
+
+export default App;
