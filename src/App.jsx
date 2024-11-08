54d42d1102f7a6c8ca1196437f97594eec212f00
Florian Schaessens <f.schaessens@gmail.com>
2024-11-08 09:30:28 +0100

    config biome json & add format script

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return <div>Coucou</div>;
+  return (
+  <div>
+    <p>Coucou</p>
+    <p>Coucou toi</p>
+  </div>
+);
 }
 
 export default App;
